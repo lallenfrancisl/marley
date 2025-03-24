d77ff984a3b65e899de74c90a6b8275e5b3abb63
Allen Francis <lallenfrancisl@gmail.com>
2025-03-24 09:58:29 +0530

    refactor: hide id from table

--- healthcare/healthcare/doctype/inpatient_record/inpatient_record_list.js
+++ healthcare/healthcare/doctype/inpatient_record/inpatient_record_list.js
@@ -2,6 +2,12 @@
 // For license information, please see license.txt
 
 frappe.listview_settings['Inpatient Record'] = {
+    refresh: function() {
+    	// Hide name filter from Listview
+    	$("div[data-fieldname = name]").hide();
+    },
+    // Hide name from Listview
+	hide_name_column: true,
 	filters: [['status', 'not in', ['Discharged']]],
 	get_indicator: function (doc) {
 		if (doc.status === 'Admission Scheduled') {
