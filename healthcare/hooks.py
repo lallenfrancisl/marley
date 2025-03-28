0473c9be7ff24e46d7d270b1a9af50327f0ca632
Allen Francis <lallenfrancisl@gmail.com>
2025-03-28 08:42:34 +0530

    feat: update colors

--- healthcare/hooks.py
+++ healthcare/hooks.py
@@ -14,7 +14,7 @@ required_apps = ["erpnext"]
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/healthcare/css/healthcare.css"
+app_include_css = "healthcare.bundle.css"
 app_include_js = "healthcare.bundle.js"
 
 # include js, css files in header of web template
