da0c4439c3c35efec7d7fc86b77a10fcd016c926
Allen Francis <lallenfrancisl@gmail.com>
2025-04-02 15:58:31 +0530

    fix: update the columns appearing in patient appointment list view

--- healthcare/healthcare/doctype/patient_appointment/patient_appointment_list.js
+++ healthcare/healthcare/doctype/patient_appointment/patient_appointment_list.js
@@ -2,19 +2,26 @@
 (c) ESS 2015-16
 */
 frappe.listview_settings["Patient Appointment"] = {
+	add_fields: ["patient", "status", "date", "appointment_type"],
+	refresh: function () {
+		$("div.level-right").each(function () {
+			$(this).hide();
+		});
+	},
 	filters: [["status", "=", "Open"]],
-	get_indicator: function(doc) {
+	hide_name_column: true,
+	get_indicator: function (doc) {
 		var colors = {
-			"Open": "orange",
-			"Scheduled": "yellow",
-			"Closed": "green",
-			"Cancelled": "red",
-			"Expired": "grey",
+			Open: "orange",
+			Scheduled: "yellow",
+			Closed: "green",
+			Cancelled: "red",
+			Expired: "grey",
 			"Checked In": "blue",
 			"Checked Out": "orange",
-			"Confirmed": "green",
-			"No Show": "red"
+			Confirmed: "green",
+			"No Show": "red",
 		};
 		return [__(doc.status), colors[doc.status], "status,=," + doc.status];
-	}
+	},
 };
