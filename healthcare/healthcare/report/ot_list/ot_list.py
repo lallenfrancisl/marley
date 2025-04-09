4133e90cd758da4aa4d9c2e2b0c096438fc5de6f
Allen Francis <lallenfrancisl@gmail.com>
2025-04-09 07:14:54 +0530

    fix: error with ip not found

--- healthcare/healthcare/report/ot_list/ot_list.py
+++ healthcare/healthcare/report/ot_list/ot_list.py
@@ -68,116 +68,132 @@ def execute(filters=None):
 
 	for item in procedures:
 		patient = frappe.get_doc("Patient", item.patient)
-		ip_record = None
-
-		try:
-			ip_record = frappe.get_doc("Inpatient Record", item.inpatient_record)
-		except:
-			ip_record = None
 
 		if not patient:
 			continue
 
-		name = f"<strong>{patient.patient_name}</strong><br><br>"
+		is_inpatient = frappe.db.exists("Inpatient Record", item.inpatient_record)
 
-		if patient.surgical_history:
-			name += f"""
-				<div>
-					Surgical History
-				</div>
-				<div>
-					{new_lines_to_br(patient.surgical_history)}
-				</div>
-				<br>
-			"""
-		
-		if patient.medical_history:
-			name += f"""
-				<div>
-					Medical History
-				</div>
-				<div>
-					{new_lines_to_br(patient.medical_history)}
-				</div>
-				<br>
-			"""
+		if is_inpatient:
+			ip_record = frappe.get_doc("Inpatient Record", item.inpatient_record)
 
-		if patient.medication:
-			name += f"""
-				<div>
-					Medications
-				</div>
-				<div>
-					{new_lines_to_br(patient.medication)}
-				</div>
-				<br>
-			"""
+			name = f"<strong>{patient.patient_name}</strong><br><br>"
 
-		if patient.surrounding_factors:
-			name += f"""
-				<div>
-					Surrounding factors
-				</div>
-				<div>
-					{new_lines_to_br(patient.surrounding_factors)}
-				</div>
-				<br>
-			"""
+			if patient.surgical_history:
+				name += f"""
+					<div>
+						Surgical History
+					</div>
+					<div>
+						{new_lines_to_br(patient.surgical_history)}
+					</div>
+					<br>
+				"""
+			
+			if patient.medical_history:
+				name += f"""
+					<div>
+						Medical History
+					</div>
+					<div>
+						{new_lines_to_br(patient.medical_history)}
+					</div>
+					<br>
+				"""
 
-		if patient.other_risk_factors:
-			name += f"""
-				<div>
-					Other risk factors
-				</div>
-				<div>
-					{new_lines_to_br(patient.other_risk_factors)}
-				</div>
-				<br>
-			"""
-		
-		if patient.tobacco_past_use or patient.tobacco_current_use:
-			name += f"""
-				Tobacco Consumption(Past): {new_lines_to_br(patient.tobacco_past_use)}
-				<br>
-				Tobacco Consumption(Present): {new_lines_to_br(patient.tobacco_current_use)}
-				<br>
-			"""
+			if patient.medication:
+				name += f"""
+					<div>
+						Medications
+					</div>
+					<div>
+						{new_lines_to_br(patient.medication)}
+					</div>
+					<br>
+				"""
 
-		if patient.alcohol_past_use or patient.alcohol_current_use:
-			name += f"""
-				Alcohol Consumption(Past): {new_lines_to_br(patient.alcohol_past_use)}
-				<br>
-				Alcohol Consumption(Present): {new_lines_to_br(patient.alcohol_current_use)}
-			"""
-		
-		other_tests = []
-		for recording in ip_record.other_tests:
-			test = frappe.get_doc("Lab Test", recording.lab_test)
-			other_tests.append(
-				f"<strong>{test.template} ({frappe.format(test.result_date, { 'fieldtype': 'Date'})})</strong>: {sanitize(test.custom_result) if test.custom_result else '---'}"
-			)
+			if patient.surrounding_factors:
+				name += f"""
+					<div>
+						Surrounding factors
+					</div>
+					<div>
+						{new_lines_to_br(patient.surrounding_factors)}
+					</div>
+					<br>
+				"""
 
-		diagnosis = f'<strong>{(" with ".join(map(lambda x: x.diagnosis, ip_record.diagnosis)) if len(ip_record.diagnosis) else "")}</strong><br>'
+			if patient.other_risk_factors:
+				name += f"""
+					<div>
+						Other risk factors
+					</div>
+					<div>
+						{new_lines_to_br(patient.other_risk_factors)}
+					</div>
+					<br>
+				"""
+			
+			if patient.tobacco_past_use or patient.tobacco_current_use:
+				name += f"""
+					Tobacco Consumption(Past): {new_lines_to_br(patient.tobacco_past_use)}
+					<br>
+					Tobacco Consumption(Present): {new_lines_to_br(patient.tobacco_current_use)}
+					<br>
+				"""
 
-		for test in other_tests:
-			diagnosis += f"{test}<br>"
-		
-		data.append({
-			"name": patient.patient_name,
-			"abssby_no": patient.abssby or "NO",
-			"age_sex": f"{patient.age.years}/{patient.sex}",
-			"ipd_no": ip_record.cr_number if ip_record else "",
-			"diagnosis": (" with ".join(map(lambda x: x.diagnosis, ip_record.diagnosis)) if ip_record and len(ip_record.diagnosis) else ""),
-			"operations": item.procedure_template,
-			"formatted": {
-				"name": name,
+			if patient.alcohol_past_use or patient.alcohol_current_use:
+				name += f"""
+					Alcohol Consumption(Past): {new_lines_to_br(patient.alcohol_past_use)}
+					<br>
+					Alcohol Consumption(Present): {new_lines_to_br(patient.alcohol_current_use)}
+				"""
+			
+			other_tests = []
+			for recording in ip_record.other_tests:
+				test = frappe.get_doc("Lab Test", recording.lab_test)
+				other_tests.append(
+					f"<strong>{test.template} ({frappe.format(test.result_date, { 'fieldtype': 'Date'})})</strong>: {sanitize(test.custom_result) if test.custom_result else '---'}"
+				)
+
+			diagnosis = f'<strong>{(" with ".join(map(lambda x: x.diagnosis, ip_record.diagnosis)) if len(ip_record.diagnosis) else "")}</strong><br>'
+
+			for test in other_tests:
+				diagnosis += f"{test}<br>"
+			
+			data.append({
+				"name": patient.patient_name,
 				"abssby_no": patient.abssby or "NO",
 				"age_sex": f"{patient.age.years}/{patient.sex}",
-				"ipd_no": ip_record.cr_number if ip_record else "", 
-				"diagnosis": diagnosis,
-				"operations": f"<strong>{item.procedure_template}</strong>",
-			}
-		})
+				"ipd_no": ip_record.cr_number if ip_record else "",
+				"diagnosis": (" with ".join(map(lambda x: x.diagnosis, ip_record.diagnosis)) if ip_record and len(ip_record.diagnosis) else ""),
+				"operations": item.procedure_template,
+				"formatted": {
+					"name": name,
+					"abssby_no": patient.abssby or "NO",
+					"age_sex": f"{patient.age.years}/{patient.sex}",
+					"ipd_no": ip_record.cr_number if ip_record else "", 
+					"diagnosis": diagnosis,
+					"operations": f"<strong>{item.procedure_template}</strong>",
+				}
+			})
+		else:
+			data.append({
+				"name": patient.patient_name,
+				"abssby_no": patient.abssby or "NO",
+				"age_sex": f"{patient.age.years}/{patient.sex}",
+				"ipd_no": "",
+				"diagnosis": "",
+				"operations": item.procedure_template,
+				"formatted": {
+					"name": name,
+					"abssby_no": patient.abssby or "NO",
+					"age_sex": f"{patient.age.years}/{patient.sex}",
+					"ipd_no": "", 
+					"diagnosis": "",
+					"operations": f"<strong>{item.procedure_template}</strong>",
+				}
+			})
 
 	return columns, data, filters
 
